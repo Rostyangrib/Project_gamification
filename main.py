113fbeeced817c9cc0ce74a43b44bcac49460d41
maxdaul <maxdaul90@gmail.com>
2025-12-26 01:31:12 +0800

    Комментарии к  main.py

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ app = FastAPI(
 
 app.include_router(router)
 
+#Отдача статических файлов из папки assets
 app.mount("/assets", StaticFiles(directory="frontend/dist/assets"), name="assets")
 
-
+#Отдает конкретный файл если путь указывает на него или отдает index(React)
 @app.get("/{full_path:path}")
 async def serve_frontend(full_path: str = ""):
     if full_path and os.path.exists(f"frontend/dist/{full_path}"):
         return FileResponse(f"frontend/dist/{full_path}")
 
     return FileResponse("frontend/dist/index.html")
+
